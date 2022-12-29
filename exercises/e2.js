4c64af4638d9792a0e85d96e5b88fe40945ab933
Nick Hoang <nickjhoang@gmail.com>
2022-12-29 12:13:51 -0600

    complete solution exercise 2

--- exercises/e2.js
+++ exercises/e2.js
@@ -5,7 +5,10 @@ import { data } from "../data/data";
 // Return example: ['name1', 'name2', ... , 'nameN']
 
 export function getAsteroidNames(data) {
-  // Your code goes here...
+  let asteroidNames = [];
+  const asteroids = data.asteroids;
+  asteroidNames = asteroids.map(obj => obj.name);
+  return asteroidNames;
 }
 
 
