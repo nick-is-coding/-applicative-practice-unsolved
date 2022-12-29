b498a929b0fb9a0138c6b8e2f5b6bc4d0ea46b70
Nick Hoang <nickjhoang@gmail.com>
2022-12-29 15:41:00 -0600

    complete solution exercise 7

--- exercises/e7.js
+++ exercises/e7.js
@@ -5,7 +5,11 @@ import { data } from "../data/data";
 // Return example: ['name1', 'name2', ... , 'nameN']
 
 export function getPlanetsNamesWithMoons(data) {
-  // Your code goes here...
+  let moonPlanets = [];
+  const planets = data.planets;
+  const moonsPresent = planets.filter(obj => obj.moonsCount > 0);
+  moonPlanets = moonsPresent.map(obj => obj.name);
+  return moonPlanets;
 }
 
 
