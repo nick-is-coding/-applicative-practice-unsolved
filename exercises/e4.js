dd0e409b6f00cc6eb1357e169b874b408c1037f4
Nick Hoang <nickjhoang@gmail.com>
2022-12-29 12:47:11 -0600

    complete solution exercise 4

--- exercises/e4.js
+++ exercises/e4.js
@@ -5,9 +5,12 @@ import { data } from "../data/data";
 // Return example: ['name1', 'name2', ... , 'nameN']
 
 export function getPlanetsWithLowGravity(data) {
-  // Your code goes here...
+  let lowGravityPlanets = [];
+  const planets = data.planets;
+  const lowGravity = planets.filter(obj => obj.gravity < 10);
+  lowGravityPlanets = lowGravity.map(obj => obj.name);
+  return lowGravityPlanets;
 }
-
 
 
 // === TEST YOURSELF ===
