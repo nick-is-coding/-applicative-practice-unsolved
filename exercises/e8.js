8ce9e9742883eca5cf8fab7b50b54ab30b75a642
Nick Hoang <nickjhoang@gmail.com>
2022-12-30 16:51:04 -0600

    complete solution exercise 8

--- exercises/e8.js
+++ exercises/e8.js
@@ -5,10 +5,9 @@ import { data } from "../data/data";
 // Return example: 'Planet Name'
 
 export function findPlanetNameByMoon(data, moonName) {
-  // Your code goes here...
+  const planet = data.planets.find(planet => planet.moons.includes(moonName));
+  return planet.name;
 }
-
-
 
 // === TEST YOURSELF ===
 // Once you're finished run the test with "npm run test-8"
