1f129f4e2052ff315cae00ebb040f446954a0b3f
Nick Hoang <nickjhoang@gmail.com>
2022-12-30 18:52:01 -0600

    complete solution exercise 14

--- exercises/e14.js
+++ exercises/e14.js
@@ -5,7 +5,8 @@ import { data } from "../data/data";
 // Return example: 234234.234
 
 export function getOrbitalPeriodsSum(data) {
-  // Your code goes here...
+  return data.asteroids.map(obj => obj.orbitalPeriod)
+  .reduce((sum, period) => sum + period, 0);
 }
 
 
